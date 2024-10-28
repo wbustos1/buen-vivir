a1b02f67c356f4ada4fcbead41463b8943a0b550
Wilson Bustos <wrbutros@gmail.com>
2024-10-28 20:48:46 +0800

    Add carrusel

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const $ = (selector) => document.querySelector(selector);
 
+const IMG_DIR = "./Images/carrusel";
+const IMG_NAMES = [
+  "img1.png",
+  "img2.png",
+  "img3.jpg",
+  "img4.jpg",
+  "img5.jpg",
+  "img6.jpg",
+  "img7.jpg",
+  "img8.jpg",
+  "img9.jpg",
+  "img10.jpg",
+];
+
 const globalMenu_className = ".menu-global";
 const globalMenu_scrollClass = "menu-global-scrolled";
 
@@ -12,4 +26,33 @@ const onScroll_menuButtomBorder = () => {
   }
 };
 
+const createCarrusel = (imgDir, imgNameList, transitionSeconds) => {
+  const carruselDiv = $("#carrusel");
+  const imgElements = [];
+  imgNameList.forEach((imgName) => {
+    const img = document.createElement("img");
+    img.src = `${imgDir}/${imgName}`;
+    img.className = "invisible";
+    imgElements.push(img);
+    carruselDiv.appendChild(img);
+  });
+
+  let currentIdx = 0;
+
+  const showNextImg = () => {
+    const prevIdx = currentIdx === 0 ? imgNameList.length - 1 : currentIdx - 1;
+
+    imgElements[prevIdx].className = "invisible";
+    imgElements[currentIdx].className = "";
+
+    currentIdx = currentIdx === imgNameList.length - 1 ? 0 : currentIdx + 1;
+  };
+
+  showNextImg();
+  setInterval(showNextImg, 1000 * transitionSeconds);
+};
+
 document.addEventListener("scroll", onScroll_menuButtomBorder);
+document.addEventListener("DOMContentLoaded", () => {
+  createCarrusel(IMG_DIR, IMG_NAMES, 3);
+});

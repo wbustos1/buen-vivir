fdc04dd90b2a084379c99d2e345ffb7912e51a86
Wilson Bustos <wrbutros@gmail.com>
2024-11-14 18:07:08 +0800

    Add Text and accurate images

--- constants.js
+++ constants.js
@@ -11,34 +11,63 @@ export const IMG_NAMES = [
   "img9.jpg",
   "img10.jpg",
   "img11.jpg",
+  "img12.jpg",
 ];
 
 export const TEXT_CARRUSEL = [
   [
     {
-      title: "Titulo 1-1",
-      content: `Contenido de prueba 1`,
+      title: "¡Bienvenidos!",
+      content: `Casa Residencial Buen vivir ofrece servicios de larga estadía para personas de tercera edad`,
     },
     {
-      title: "Titulo 1-2",
-      content: `Contenido de prueba 2`,
+      title: "Localidad",
+      content: `Nos ubicamos en Januario Espinoza 7071, Comuna la Reina. A tan solo dos cuadras de la Av. José Arrieta y a una cuadra de la Av. José Arrieta`,
     },
     {
-      title: "Titulo 1-3",
-      content: `Contenido de prueba 3`,
+      title: "Instalaciones",
+      content: `Prueba 3`,
     },
   ],
   [
     {
-      title: "Titulo 2-1",
-      content: `Contenido de prueba 4`,
+      title: "Atención Personalizada",
+      content: `Tenemos un cupo máximo de 6 personas a quienes atendemos de forma personalizada con el fin de maximizar su comodidad lo máximo posible.`,
     },
     {
-      title: "Titulo 2-2",
+      title: "Actividades",
       content: `Contenido de prueba 5`,
     },
     {
-      title: "Titulo 2-3",
+      title: "Entretenimiento",
+      content: `Contenido de prueba 6`,
+    },
+  ],
+  [
+    {
+      title: "Asistencia 24/7",
+      content: `Contenido de prueba 4`,
+    },
+    {
+      title: "Asistencia de adminsitración de medicamentos",
+      content: `Contenido de prueba 5`,
+    },
+    {
+      title: "Horarios de comida",
+      content: `Contenido de prueba 6`,
+    },
+  ],
+  [
+    {
+      title: "Habitaciones acondicionadas",
+      content: `Contenido de prueba 4`,
+    },
+    {
+      title: "Sanitarios adaptados a personas de mobilidad reducida",
+      content: `Contenido de prueba 5`,
+    },
+    {
+      title: "Visitas sin restricciones",
       content: `Contenido de prueba 6`,
     },
   ],
